50e1f2eba74f45bc2bd1473d1fcf9d5c69938bc6
flake @ pc <91631241+svntythsnd@users.noreply.github.com>
2025-11-17 23:39:31 +0100

    v0.1.27
    - fixed map image
    - added calcis nag to bottom of FALLHUB
    - made rise03 token saint inaccessible

--- source/UQLTerminus.cs
+++ source/UQLTerminus.cs
@@ -7,7 +7,7 @@ using BepInEx.Logging;
 
 namespace UQLTerminus;
 
-[BepInPlugin("uql.terminus", "Local Terminus", "0.1.26")]
+[BepInPlugin("uql.terminus", "Local Terminus", "0.1.27")]
 public partial class UQLTerminus : BaseUnityPlugin
 {
 
