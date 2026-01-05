71dd58b300415a79285f7a44bddbebe9361f7686
flake @ pc <91631241+svntythsnd@users.noreply.github.com>
2026-01-05 23:49:55 +0100

    v0.1.29
    - made shelters floodsafe
    - added pipe sounds to JX_RISE02

--- source/UQLTerminus.cs
+++ source/UQLTerminus.cs
@@ -7,7 +7,7 @@ using BepInEx.Logging;
 
 namespace UQLTerminus;
 
-[BepInPlugin("uql.terminus", "Local Terminus", "0.1.28")]
+[BepInPlugin("uql.terminus", "Local Terminus", "0.1.29")]
 public partial class UQLTerminus : BaseUnityPlugin
 {
 
